df26a1d37b7ae180680572ee2bcb5861d24df57a
Willi Schinmeyer <willi@schinmeyer.de>
2017-07-23 17:51:37 +0200

    rename MainPage to TaskListPage

--- TaskList.xaml.cs
+++ TaskList.xaml.cs
@@ -20,9 +20,9 @@ namespace WPTaskClient
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class TaskListPage : Page
     {
-        public MainPage()
+        public TaskListPage()
         {
             this.InitializeComponent();
         }
