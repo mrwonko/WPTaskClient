0b1695ff8df27e6275fc95a6bd574f474e46e8e2
Willi Schinmeyer <willi@schinmeyer.de>
2017-07-28 11:59:04 +0200

    add & test Task class

---
+++ WPTaskClient/Data/Task.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Json = Windows.Data.Json;
+
+namespace WPTaskClient
+{
+    namespace Data
+    {
+        public class Task
+        {
+            public enum Status
+            {
+                Pending,
+                Deleted,
+                Completed,
+                Waiting,
+                Recurring,
+            }
+
+            private static readonly string dateFormat = "yyyyMMdd'T'HHmmss'Z'";
+            private static readonly Dictionary<string, Status> statusLUT = new Dictionary<string, Status>
+            {
+                { "pending", Status.Pending },
+                { "deleted", Status.Deleted },
+                { "completed", Status.Completed },
+                { "waiting", Status.Waiting },
+                { "recurring", Status.Recurring },
+            };
+
+            private Guid uuid;
+            private Status status;
+            private String description;
+            private DateTime lastModified; // UTC
+            private Dictionary<string, Json.IJsonValue> additionalAttributes;
+
+            public Task(Guid uuid, Status status, string description, DateTime lastModified, Dictionary<string, Json.IJsonValue> additionalAttributes)
+            {
+                this.uuid = uuid;
+                this.status = status;
+                this.description = description;
+                this.lastModified = lastModified;
+                this.additionalAttributes = additionalAttributes;
+            }
+
+            public static Task New(string description)
+            {
+                return new Task(Guid.NewGuid(), Status.Pending, description, DateTime.UtcNow, new Dictionary<string, Json.IJsonValue>());
+            }
+
+            private static readonly HashSet<string> handledAttributes = new HashSet<string> { "uuid", "status", "description", "modified" };
+            public static Task FromJson(Json.JsonObject json)
+            {
+                var uuid = Guid.Parse(json.GetNamedString("uuid"));
+                var status = statusLUT[json.GetNamedString("status")];
+                var description = json.GetNamedString("description");
+                var lastModified = DateTime.ParseExact(json.GetNamedString("modified"), dateFormat, System.Globalization.CultureInfo.InvariantCulture);
+                var additionalAttributes = new Dictionary<string, Json.IJsonValue>();
+                foreach (var entry in json)
+                {
+                    if (!handledAttributes.Contains(entry.Key))
+                    {
+                        additionalAttributes.Add(entry.Key, entry.Value);
+                    }
+
+                }
+                return new Task(uuid, status, description, lastModified, additionalAttributes);
+            }
+
+            public Json.JsonObject ToJson()
+            {
+                var result = new Json.JsonObject();
+                result.Add("uuid", Json.JsonValue.CreateStringValue(uuid.ToString()));
+                result.Add("status", Json.JsonValue.CreateStringValue(this.status.ToString().ToLower()));
+                result.Add("description", Json.JsonValue.CreateStringValue(this.description));
+                result.Add("modified", Json.JsonValue.CreateStringValue(this.lastModified.ToString(dateFormat)));
+                foreach (var attribute in this.additionalAttributes)
+                {
+                    result.Add(attribute.Key, attribute.Value);
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ WPTaskClientTests/UnitTest.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Json = Windows.Data.Json;
+
+namespace WPTaskClientTests
+{
+    [TestClass]
+    public class Task
+    {
+        [TestMethod]
+        public void TestToJson()
+        {
+            // given
+            var rawUUID = "296d835e-8f85-4224-8f36-c612cad1b9f8";
+            var description = "test";
+            var date = new DateTime(2017, 7, 25, 21, 29, 0);
+            var expectedModifiedDate = "20170725T212900Z";
+            var additionalAttributes = new Dictionary<string, Json.IJsonValue>
+            {
+                { "estimate", Json.JsonValue.CreateStringValue("1w") }
+            };
+            // when
+            var entry = new WPTaskClient.Data.Task(Guid.Parse(rawUUID), WPTaskClient.Data.Task.Status.Pending, description, date, additionalAttributes);
+            var json = entry.ToJson();
+            // then
+            AssertStringValue(json, "uuid", rawUUID);
+            AssertStringValue(json, "status", "pending");
+            AssertStringValue(json, "modified", expectedModifiedDate);
+            AssertStringValue(json, "description", description);
+            AssertStringValue(json, "estimate", "1w");
+        }
+
+        [TestMethod]
+        public void TestFromJson()
+        {
+            // given
+            var rawJson = @"{""uuid"":""296d835e-8f85-4224-8f36-c612cad1b9f8"",""status"":""pending"",""modified"":""20170725T221600Z"",""description"":""test task"",""uda"":""user-defined attribute""}";
+            var givenJson = Json.JsonObject.Parse(rawJson);
+            // when
+            var entry = WPTaskClient.Data.Task.FromJson(givenJson);
+            var json = entry.ToJson();
+            // then
+            AssertJsonEqual(givenJson, json);
+        }
+
+        private static void AssertStringValue(Json.JsonObject obj, string field, string expected)
+        {
+            Assert.IsTrue(obj.TryGetValue(field, out Json.IJsonValue value), "object {0} must contain [1] field", obj, field);
+            StringAssert.Equals(value.GetString(), expected);
+        }
+
+        private static void AssertJsonEqual(Json.IJsonValue lhs, Json.IJsonValue rhs, string path = "")
+        {
+            Assert.AreEqual(lhs.ValueType, rhs.ValueType, path);
+            switch(lhs.ValueType)
+            {
+                case Json.JsonValueType.Null:
+                    // null always equal
+                    break;
+                case Json.JsonValueType.Boolean:
+                    Assert.AreEqual(lhs.GetBoolean(), rhs.GetBoolean(), path);
+                    break;
+                case Json.JsonValueType.Number:
+                    Assert.AreEqual(lhs.GetNumber(), rhs.GetNumber(), path);
+                    break;
+                case Json.JsonValueType.String:
+                    Assert.AreEqual(lhs.GetString(), rhs.GetString(), path);
+                    break;
+                case Json.JsonValueType.Array:
+                    var lhsArr = lhs.GetArray();
+                    var rhsArr = rhs.GetArray();
+                    Assert.AreEqual(lhsArr.Count, rhsArr.Count, path + ".length");
+                    for(var i = 0; i < lhsArr.Count; i++)
+                    {
+                        AssertJsonEqual(lhsArr[i], rhsArr[i], String.Format("{0}[{1}]", path, i));
+                    }
+                    break;
+                case Json.JsonValueType.Object:
+                    var lhsObj = lhs.GetObject();
+                    var rhsObj = rhs.GetObject();
+                    Assert.AreEqual(lhsObj.Count, rhsObj.Count, String.Format("len({0})", path));
+                    foreach(var lhsEntry in lhsObj) {
+                        AssertJsonEqual(lhsEntry.Value, rhsObj[lhsEntry.Key]);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
