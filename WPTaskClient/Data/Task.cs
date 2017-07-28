4811e63d19494da29cc302d491b2219df993418d
Willi Schinmeyer <willi@schinmeyer.de>
2017-07-28 14:41:40 +0200

    add Task Tags and make Task immutable

--- WPTaskClient/Data/Task.cs
+++ WPTaskClient/Data/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace WPTaskClient
                 Waiting,
                 Recurring,
             }
-            
+
             private static readonly Dictionary<string, Status> statusLUT = new Dictionary<string, Status>
             {
                 { "pending", Status.Pending },
@@ -29,28 +30,40 @@ namespace WPTaskClient
                 { "recurring", Status.Recurring },
             };
 
-            private Guid uuid;
-            private Status status;
-            private String description;
-            private Timestamp entered;
-            private Timestamp lastModified;
-            private static readonly HashSet<string> handledAttributes = new HashSet<string> { "uuid", "status", "description", "entry", "modified" };
-            private Dictionary<string, Json.IJsonValue> additionalAttributes;
+            private readonly Guid uuid;
+            private readonly Status status;
+            private readonly String description;
+            private readonly Timestamp entered;
+            private readonly Timestamp lastModified;
+            private readonly ImmutableList<string> tags;
+            private static readonly ImmutableHashSet<string> handledAttributes =
+                ImmutableHashSet.Create("uuid", "status", "description", "entry", "modified", "tags");
+            private readonly ImmutableDictionary<string, Json.IJsonValue> additionalAttributes;
 
-            public Task(Guid uuid, Status status, string description, Timestamp entered, Timestamp lastModified, Dictionary<string, Json.IJsonValue> additionalAttributes)
+            private static ImmutableList<string> noTags =
+                ImmutableList.Create<string>();
+            private static ImmutableDictionary<string, Json.IJsonValue> noAdditionalAttributes =
+                ImmutableDictionary.Create<string, Json.IJsonValue>();
+
+            public Task(Guid uuid, Status status, string description, Timestamp entered, Timestamp lastModified, ImmutableList<string> tags, ImmutableDictionary<string, Json.IJsonValue> additionalAttributes)
             {
+                if (tags.Any(tag => tag.Any(char.IsWhiteSpace)))
+                {
+                    throw new System.ArgumentException("tag contains whitespace", "tags");
+                }
                 this.uuid = uuid;
                 this.status = status;
                 this.description = description;
                 this.entered = entered;
                 this.lastModified = lastModified;
+                this.tags = tags;
                 this.additionalAttributes = additionalAttributes;
             }
 
-            public static Task New(string description)
+            public static Task New(string description, ImmutableList<string> tags)
             {
                 var now = Timestamp.Now;
-                return new Task(Guid.NewGuid(), Status.Pending, description, now, now, new Dictionary<string, Json.IJsonValue>());
+                return new Task(Guid.NewGuid(), Status.Pending, description, now, now, tags, noAdditionalAttributes);
             }
 
             public static Task FromJson(Json.JsonObject json)
@@ -60,26 +73,31 @@ namespace WPTaskClient
                 var description = json.GetNamedString("description");
                 var entered = new Timestamp(json.GetNamedString("entry"));
                 var lastModified = new Timestamp(json.GetNamedString("modified"));
-                var additionalAttributes = new Dictionary<string, Json.IJsonValue>();
-                foreach (var entry in json)
+                var tags = noTags;
+                if (json.ContainsKey("tags"))
                 {
-                    if (!handledAttributes.Contains(entry.Key))
-                    {
-                        additionalAttributes.Add(entry.Key, entry.Value);
-                    }
-
+                    tags = ImmutableList.CreateRange(json.GetNamedArray("tags").Select(value => value.GetString()));
                 }
-                return new Task(uuid, status, description, entered, lastModified, additionalAttributes);
+                var additionalAttributes = ImmutableDictionary.CreateRange(json.Where(entry => !handledAttributes.Contains(entry.Key)));
+                return new Task(uuid, status, description, entered, lastModified, tags, additionalAttributes);
             }
 
             public Json.JsonObject ToJson()
             {
-                var result = new Json.JsonObject();
-                result.Add("uuid", Json.JsonValue.CreateStringValue(uuid.ToString()));
-                result.Add("status", Json.JsonValue.CreateStringValue(this.status.ToString().ToLower()));
-                result.Add("description", Json.JsonValue.CreateStringValue(this.description));
-                result.Add("entry", Json.JsonValue.CreateStringValue(this.entered.ToString()));
-                result.Add("modified", Json.JsonValue.CreateStringValue(this.lastModified.ToString()));
+                var result = new Json.JsonObject
+                {
+                    { "uuid", Json.JsonValue.CreateStringValue(uuid.ToString()) },
+                    { "status", Json.JsonValue.CreateStringValue(status.ToString().ToLower()) },
+                    { "description", Json.JsonValue.CreateStringValue(description) },
+                    { "entry", Json.JsonValue.CreateStringValue(entered.ToString()) },
+                    { "modified", Json.JsonValue.CreateStringValue(lastModified.ToString()) }
+                };
+                if (!tags.IsEmpty)
+                {
+                    var tags = new Json.JsonArray();
+                    this.tags.ForEach(tag => tags.Add(Json.JsonValue.CreateStringValue(tag)));
+                    result.Add("tags", tags);
+                }
                 foreach (var attribute in this.additionalAttributes)
                 {
                     result.Add(attribute.Key, attribute.Value);
--- WPTaskClientTests/UnitTest.cs
+++ WPTaskClientTests/UnitTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Json = Windows.Data.Json;
+using System.Collections.Immutable;
 
 namespace WPTaskClientTests
 {
@@ -17,41 +18,47 @@ namespace WPTaskClientTests
             var description = "test";
             var enteredDate = new WPTaskClient.Data.Timestamp(new DateTime(2017, 7, 25, 21, 29, 0));
             var modifiedDate = new WPTaskClient.Data.Timestamp(new DateTime(2017, 7, 28, 13, 25, 0));
-            var expectedEnteredDate = "20170728T132500Z";
-            var expectedModifiedDate = "20170725T212900Z";
-            var additionalAttributes = new Dictionary<string, Json.IJsonValue>
+            var tags = new List<string>() { "first", "second" }.ToImmutableList();
+            var additionalAttributes = new Dictionary<string, Json.IJsonValue>()
             {
-                { "estimate", Json.JsonValue.CreateStringValue("1w") }
-            };
+                { "estimate", Json.JsonValue.CreateStringValue("1w")}
+            }.ToImmutableDictionary();
             // when
-            var entry = new WPTaskClient.Data.Task(Guid.Parse(rawUUID), WPTaskClient.Data.Task.Status.Pending, description, enteredDate, modifiedDate, additionalAttributes);
+            var entry = new WPTaskClient.Data.Task(Guid.Parse(rawUUID), WPTaskClient.Data.Task.Status.Pending, description, enteredDate, modifiedDate, tags, additionalAttributes);
             var json = entry.ToJson();
             // then
-            AssertStringValue(json, "uuid", rawUUID);
-            AssertStringValue(json, "status", "pending");
-            AssertStringValue(json, "entry", expectedEnteredDate);
-            AssertStringValue(json, "modified", expectedModifiedDate);
-            AssertStringValue(json, "description", description);
-            AssertStringValue(json, "estimate", "1w");
+            var expectedJson = new Json.JsonObject()
+            {
+                { "uuid", Json.JsonValue.CreateStringValue(rawUUID) },
+                { "status", Json.JsonValue.CreateStringValue("pending") },
+                { "entry", Json.JsonValue.CreateStringValue( "20170725T212900Z") },
+                { "modified", Json.JsonValue.CreateStringValue( "20170728T132500Z") },
+                { "description", Json.JsonValue.CreateStringValue(description) },
+                { "estimate", Json.JsonValue.CreateStringValue("1w") },
+                { "tags", new Json.JsonArray(){ Json.JsonValue.CreateStringValue("first"), Json.JsonValue.CreateStringValue("second") } },
+            };
+            AssertJsonEqual(json, expectedJson);
         }
 
         [TestMethod]
         public void TestFromJson()
         {
             // given
-            var rawJson = @"{""uuid"":""296d835e-8f85-4224-8f36-c612cad1b9f8"",""status"":""pending"",""entry"":""20170725T221600Z"",""modified"":""20170728T132500Z"",""description"":""test task"",""uda"":""user-defined attribute""}";
+            var rawJson = @"
+            { ""uuid"":        ""296d835e-8f85-4224-8f36-c612cad1b9f8""
+            , ""status"":      ""pending""
+            , ""entry"":       ""20170725T221600Z""
+            , ""modified"":    ""20170728T132500Z""
+            , ""description"": ""test task""
+            , ""tags"":        [ ""foo"", ""bar"" ]
+            , ""uda"":         ""user-defined attribute""
+            }";
             var givenJson = Json.JsonObject.Parse(rawJson);
             // when
             var entry = WPTaskClient.Data.Task.FromJson(givenJson);
             var json = entry.ToJson();
             // then
             AssertJsonEqual(givenJson, json);
-        }
-
-        private static void AssertStringValue(Json.JsonObject obj, string field, string expected)
-        {
-            Assert.IsTrue(obj.TryGetValue(field, out Json.IJsonValue value), "object {0} must contain [1] field", obj, field);
-            StringAssert.Equals(value.GetString(), expected);
         }
 
         private static void AssertJsonEqual(Json.IJsonValue lhs, Json.IJsonValue rhs, string path = "")
