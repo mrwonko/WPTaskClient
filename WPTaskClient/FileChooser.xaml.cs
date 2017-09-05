830f8c538c67db5b5f09448b8c3f77fbc186c606
Willi Schinmeyer <willi@schinmeyer.de>
2017-09-05 20:37:49 +0200

    Settings mockup with FileChooser UserControl

---
+++ WPTaskClient/FileChooser.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WPTaskClient
+{
+    public sealed partial class FileChooser : UserControl
+    {
+        public FileChooser()
+        {
+            this.InitializeComponent();
+        }
+        
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            "Description",
+            typeof(String),
+            typeof(FileChooser),
+            new PropertyMetadata("File"));
+        public string Description
+        {
+            get { return (String)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+    }
+}
