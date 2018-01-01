39a854be0269279c1ea2dfa6ed3d4d1902ca66f5
Willi Schinmeyer <willi@schinmeyer.de>
2018-01-01 19:31:07 +0100

    use shorthand for namespace of Task

--- WPTaskClient/Data/Task.cs
+++ WPTaskClient/Data/Task.cs
@@ -6,30 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using Json = Windows.Data.Json;
 
-namespace WPTaskClient
+namespace WPTaskClient.Data
 {
-    namespace Data
+    public class Task
     {
-        public class Task
+        public class ByLastModified : IComparer<Task>
         {
-            public class ByLastModified : IComparer<Task>
+            int IComparer<Task>.Compare(Task x, Task y)
             {
-                int IComparer<Task>.Compare(Task x, Task y)
-                {
-                    return x.lastModified.CompareTo(y.lastModified);
-                }
+                return x.lastModified.CompareTo(y.lastModified);
             }
+        }
 
-            public enum Status
-            {
-                Pending,
-                Deleted,
-                Completed,
-                Waiting,
-                Recurring,
-            }
+        public enum Status
+        {
+            Pending,
+            Deleted,
+            Completed,
+            Waiting,
+            Recurring,
+        }
 
-            private static readonly ImmutableDictionary<string, Status> statusLUT = new Dictionary<string, Status>
+        private static readonly ImmutableDictionary<string, Status> statusLUT = new Dictionary<string, Status>
             {
                 { "pending", Status.Pending },
                 { "deleted", Status.Deleted },
@@ -38,68 +36,68 @@ namespace WPTaskClient
                 { "recurring", Status.Recurring },
             }.ToImmutableDictionary();
 
-            private readonly Guid uuid;
-            private readonly Status status;
-            private readonly String description;
-            private readonly Timestamp entered;
-            private readonly Timestamp lastModified;
-            private readonly ImmutableList<string> tags;
-            private static readonly ImmutableHashSet<string> handledAttributes =
-                ImmutableHashSet.Create("uuid", "status", "description", "entry", "modified", "tags");
-            private readonly ImmutableDictionary<string, Json.IJsonValue> additionalAttributes;
+        private readonly Guid uuid;
+        private readonly Status status;
+        private readonly String description;
+        private readonly Timestamp entered;
+        private readonly Timestamp lastModified;
+        private readonly ImmutableList<string> tags;
+        private static readonly ImmutableHashSet<string> handledAttributes =
+            ImmutableHashSet.Create("uuid", "status", "description", "entry", "modified", "tags");
+        private readonly ImmutableDictionary<string, Json.IJsonValue> additionalAttributes;
 
-            private static ImmutableList<string> noTags =
-                ImmutableList.Create<string>();
-            private static ImmutableDictionary<string, Json.IJsonValue> noAdditionalAttributes =
-                ImmutableDictionary.Create<string, Json.IJsonValue>();
+        private static ImmutableList<string> noTags =
+            ImmutableList.Create<string>();
+        private static ImmutableDictionary<string, Json.IJsonValue> noAdditionalAttributes =
+            ImmutableDictionary.Create<string, Json.IJsonValue>();
 
-            public Guid Uuid => uuid;
-            public string Description => description;
+        public Guid Uuid => uuid;
+        public string Description => description;
 
-            public Task(Guid uuid, Status status, string description, Timestamp entered, Timestamp lastModified, ImmutableList<string> tags, ImmutableDictionary<string, Json.IJsonValue> additionalAttributes)
+        public Task(Guid uuid, Status status, string description, Timestamp entered, Timestamp lastModified, ImmutableList<string> tags, ImmutableDictionary<string, Json.IJsonValue> additionalAttributes)
+        {
+            if (description == "")
             {
-                if (description == "")
-                {
-                    throw new System.ArgumentException("description must not be empty", "description");
-                }
-                if (tags.Any(tag => tag.Any(char.IsWhiteSpace)))
-                {
-                    throw new System.ArgumentException("tag must not contain whitespace", "tags");
-                }
-                this.uuid = uuid;
-                this.status = status;
-                this.description = description;
-                this.entered = entered;
-                this.lastModified = lastModified;
-                this.tags = tags;
-                this.additionalAttributes = additionalAttributes;
+                throw new System.ArgumentException("description must not be empty", "description");
             }
-
-            public static Task New(string description, ImmutableList<string> tags)
+            if (tags.Any(tag => tag.Any(char.IsWhiteSpace)))
             {
-                var now = Timestamp.Now;
-                return new Task(Guid.NewGuid(), Status.Pending, description, now, now, tags, noAdditionalAttributes);
+                throw new System.ArgumentException("tag must not contain whitespace", "tags");
             }
+            this.uuid = uuid;
+            this.status = status;
+            this.description = description;
+            this.entered = entered;
+            this.lastModified = lastModified;
+            this.tags = tags;
+            this.additionalAttributes = additionalAttributes;
+        }
 
-            public static Task FromJson(Json.JsonObject json)
+        public static Task New(string description, ImmutableList<string> tags)
+        {
+            var now = Timestamp.Now;
+            return new Task(Guid.NewGuid(), Status.Pending, description, now, now, tags, noAdditionalAttributes);
+        }
+
+        public static Task FromJson(Json.JsonObject json)
+        {
+            var uuid = Guid.Parse(json.GetNamedString("uuid"));
+            var status = statusLUT[json.GetNamedString("status")];
+            var description = json.GetNamedString("description");
+            var entered = new Timestamp(json.GetNamedString("entry"));
+            var lastModified = new Timestamp(json.GetNamedString("modified"));
+            var tags = noTags;
+            if (json.ContainsKey("tags"))
             {
-                var uuid = Guid.Parse(json.GetNamedString("uuid"));
-                var status = statusLUT[json.GetNamedString("status")];
-                var description = json.GetNamedString("description");
-                var entered = new Timestamp(json.GetNamedString("entry"));
-                var lastModified = new Timestamp(json.GetNamedString("modified"));
-                var tags = noTags;
-                if (json.ContainsKey("tags"))
-                {
-                    tags = ImmutableList.CreateRange(json.GetNamedArray("tags").Select(value => value.GetString()));
-                }
-                var additionalAttributes = ImmutableDictionary.CreateRange(json.Where(entry => !handledAttributes.Contains(entry.Key)));
-                return new Task(uuid, status, description, entered, lastModified, tags, additionalAttributes);
+                tags = ImmutableList.CreateRange(json.GetNamedArray("tags").Select(value => value.GetString()));
             }
+            var additionalAttributes = ImmutableDictionary.CreateRange(json.Where(entry => !handledAttributes.Contains(entry.Key)));
+            return new Task(uuid, status, description, entered, lastModified, tags, additionalAttributes);
+        }
 
-            public Json.JsonObject ToJson()
-            {
-                var result = new Json.JsonObject
+        public Json.JsonObject ToJson()
+        {
+            var result = new Json.JsonObject
                 {
                     { "uuid", Json.JsonValue.CreateStringValue(uuid.ToString()) },
                     { "status", Json.JsonValue.CreateStringValue(status.ToString().ToLower()) },
@@ -107,18 +105,17 @@ namespace WPTaskClient
                     { "entry", Json.JsonValue.CreateStringValue(entered.ToString()) },
                     { "modified", Json.JsonValue.CreateStringValue(lastModified.ToString()) }
                 };
-                if (!tags.IsEmpty)
-                {
-                    var tags = new Json.JsonArray();
-                    this.tags.ForEach(tag => tags.Add(Json.JsonValue.CreateStringValue(tag)));
-                    result.Add("tags", tags);
-                }
-                foreach (var attribute in this.additionalAttributes)
-                {
-                    result.Add(attribute.Key, attribute.Value);
-                }
-                return result;
+            if (!tags.IsEmpty)
+            {
+                var tags = new Json.JsonArray();
+                this.tags.ForEach(tag => tags.Add(Json.JsonValue.CreateStringValue(tag)));
+                result.Add("tags", tags);
             }
+            foreach (var attribute in this.additionalAttributes)
+            {
+                result.Add(attribute.Key, attribute.Value);
+            }
+            return result;
         }
     }
 }
