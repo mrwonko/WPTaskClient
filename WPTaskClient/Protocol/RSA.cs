46a570fe21d2d297adc5bc0405e3a0c81025cf8c
Willi Schinmeyer <willi@schinmeyer.de>
2018-01-03 22:39:33 +0100

    remove RSA DER/ASN.1 parsing
    it does not seem to be necessary or a good idea

--- WPTaskClient/Protocol/RSA.cs
+++ WPTaskClient/Protocol/RSA.cs
@@ -22,28 +22,5 @@ namespace WPTaskClient.Protocol
             }
             return null;
         }
-
-        public static void ParseRSADERPrivateKey()
-        {
-            /* according to RFC 3447
-            RSAPrivateKey ::= SEQUENCE {
-                version           Version,
-                modulus           INTEGER,  -- n
-                publicExponent    INTEGER,  -- e
-                privateExponent   INTEGER,  -- d
-                prime1            INTEGER,  -- p
-                prime2            INTEGER,  -- q
-                exponent1         INTEGER,  -- d mod (p-1)
-                exponent2         INTEGER,  -- d mod (q-1)
-                coefficient       INTEGER,  -- (inverse of q) mod p
-                otherPrimeInfos   OtherPrimeInfos OPTIONAL
-            }
-            Version ::= INTEGER { two-prime(0), multi(1) }
-               (CONSTRAINED BY
-               {-- version must be multi if otherPrimeInfos present --})
-            */
-            var version = new ASN1.Integer("version");
-            // TODO
-        }
     }
 }
