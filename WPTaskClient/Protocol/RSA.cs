7a347c26b0d6091237fbfa8dfcc887131d362e76
Willi Schinmeyer <willi@schinmeyer.de>
2018-01-03 23:41:08 +0100

    import client certificate into app certificate store
    this appears to be the "correct" way of using certificates?

--- WPTaskClient/Protocol/RSA.cs
+++ WPTaskClient/Protocol/RSA.cs
@@ -1,9 +1,12 @@
 ﻿using System; // makes IAsyncOperation await-able
+using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Security.Cryptography;
 using Windows.Security.Cryptography.Certificates;
 using Windows.Security.Cryptography.Core;
+using Windows.Storage.Pickers;
 
 namespace WPTaskClient.Protocol
 {
@@ -11,13 +14,28 @@ namespace WPTaskClient.Protocol
     {
         public static async Task<Certificate> ReadCertificate()
         {
-            // FIXME: should not use system certificate store, it prevents submissions to Windows Store
-            foreach(var cert in await CertificateStores.FindAllAsync())
+            var picker = new FileOpenPicker();
+            picker.ViewMode = PickerViewMode.List;
+            picker.SuggestedStartLocation = PickerLocationId.Downloads;
+            picker.FileTypeFilter.Add(".pfx");
+            var file = await picker.PickSingleFileAsync();
+            var fileStream = await file.OpenReadAsync();
+            var contents = new MemoryStream();
+            await fileStream.AsStreamForRead().CopyToAsync(contents);
+            var pfxData = Encoding.ASCII.GetString(contents.ToArray());
+            var password = "TODO ask user";
+            var friendlyName = "TaskClientCert";
+            await CertificateEnrollmentManager.ImportPfxDataAsync(pfxData, password, ExportOption.NotExportable, KeyProtectionLevel.NoConsent, InstallOptions.None, friendlyName);
+            
+            //var appStore = CertificateStores.GetStoreByName(StandardCertificateStoreNames.Personal);
+            var query = new CertificateQuery();
+            query.FriendlyName = friendlyName;
+            var certs = await CertificateStores.FindAllAsync(query);
+            foreach (var cert in certs)
             {
-                // TODO: let user choose a cert
                 if(cert.HasPrivateKey)
                 {
-                    return cert; // works on my machine :P
+                    return cert;
                 }
             }
             return null;
